7d51440c9ad4742808ca09c3f740ab623ed0927d
Bappy Chandra Debnath <bappydebath2001@gmail.com>
2025-02-11 00:01:09 +0600

    added bappys 1st branch

---
+++ Data/MyDatabaseContext.cs
@@ -0,0 +1,14 @@
+﻿using HomeChefHub.Data.Entity;
+using Microsoft.EntityFrameworkCore;
+
+namespace HomeChefHub.Data
+{
+    public class MyDatabaseContext : DbContext 
+    {
+        public MyDatabaseContext(DbContextOptions<MyDatabaseContext> options) : base(options) {
+
+        }
+        public DbSet<Admin>Admins { get; set; }
+        
+    }
+}
